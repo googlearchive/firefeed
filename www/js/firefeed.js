44b0841c9a8509fbd7e0e2faac51ba95095c2fd6
Anant Narayanan <anant@kix.in>
2012-12-10 17:30:46 -0800

    Add firebase authentication support

--- www/js/firefeed.js
+++ www/js/firefeed.js
@@ -1,4 +1,7 @@
 
+var user = null;
+var firebase = null;
+
 function onLogin(silent) {
   var uid = prompt("Enter username: ", "Guest");
   $.ajax({
@@ -7,12 +10,19 @@ function onLogin(silent) {
     data: {user: uid || ""},
     dataType: "json",
     success: function(data) {
-      signedIn(data.user, data.token);
+      user = data.user;
+      var ref = new Firebase("https://firefeed.firebaseio.com/");
+      ref.auth(data.token, function(fin) {
+        if (fin) {
+          firebase = ref;
+          signedIn();
+        } else {
+          alert("Could not sign in to Firebase!");
+        }
+      });
     },
     error: function(xhr, status, error) {
-      if (!silent) {
-        alert("Error! " + error + " try again!");
-      }
+      alert("Error! " + error + " try again!");
     }
   });
 }
@@ -30,12 +40,70 @@ function onLogout() {
   });
 }
 
-function signedIn(user, token) {
+function signedIn() {
+  showSuggested();
   $("#login-box").css("display", "none");
   $("#welcome-msg").html("Welcome back to FireFeed, <b>" + user + "</b>!");
+  $("#content-box").css("display", "block");
 }
 
 function signedOut() {
   $("#login-box").css("display", "block");
   $("#welcome-msg").html("Welcome to FireFeed!");
+  $("#content-box").css("display", "none");
+
+  if (firebase) {
+    firebase.unauth();
+    firebase = null;
+  }
+}
+
+function showSuggested() {
+  firebase.child("users").on("child_added", function(childSnap) {
+    var uid = childSnap.name();
+    if (uid != user) {
+      $("<li/>")
+        .html(uid + " - <a onclick='followUser(\"" + uid + "\");'>Follow</a>")
+        .appendTo("#user-list");
+    }
+  });
+}
+
+function onSparkPost() {
+  if (!firebase) {
+    alert("Cannot post Spark without logging in first!");
+    return;
+  }
+
+  $("post-button").attr("disabled", "disabled");
+
+  var spark = {
+    author: user,
+    content: $("#new-spark").val()
+  };
+
+  var sparkRef = firebase.child("users").child(user).child("sparks");
+  sparkRef.push(spark, showPostedToast);
+}
+
+function showPostedToast(success) {
+  $("post-button").removeAttr("disabled");
+
+  var toast;
+  if (success) {
+    $("#new-spark").val("");
+    toast = $("<span class='success'/>");
+    toast.html("Successfully posted!")
+  } else {
+    toast = $("<span class='warning'/>");
+    toast.html("Error while posting!");
+  }
+  toast.css("display", "none");
+
+  toast.appendTo($("#submit-box"));
+  toast.delay(100).fadeIn("slow", function() {
+    $(this).delay(1000).fadeOut("slow", function() {
+      $(this).remove();
+    });
+  });
 }
