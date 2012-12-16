aff3170707c62a2ad6a659b56437181557282bfc
Anant Narayanan <anant@kix.in>
2012-12-16 14:50:17 -0800

    Add page curl

--- www/js/firefeed-ui.js
+++ www/js/firefeed-ui.js
@@ -1,30 +1,58 @@
 
-var firefeed;
+var __ff_ui;
+$(window).bind("popstate", function(e)) {
+  if (e.state) {
+    switch (e.state.page) {
+      case "home":
+        __ff_ui.renderHome();
+      case "timeline":
+        __ff_ui.renderTimeline(e.state.name);
+        break;
+    }
+  } else {
+    renderHome();
+  }
+}
 $(document).ready(function() {
-  firefeed = new Firefeed("http://firefeed.firebaseio-staging.com/");
-  renderHome();
+  __ff_ui = new FirefeedUI();
+  __ff_ui.renderHome();
 });
 
-function renderHome() {
+function FirefeedUI() {
+  this._firefeed = Firefeed("http://firefeed.firebaseio-staging.com/");
+}
+
+FirefeedUI.prototype.renderHome = function() {
   $("#header").html($("#tmpl-index-header").html());
+
+  $(".ribbon-curl").find("img").hover(function(){
+    $(this).attr("src", "img/curl-animate.gif");
+  }, function(){
+    $(this).attr("src", "img/curl-static.gif");
+  });
+
   var body = Mustache.to_html($("#tmpl-content").html(), {
     classes: "cf home", content: $("#tmpl-index-content").html()
   });
   $("#body").html(body); 
 
+  var self = this;
   $("#login-button").click(function(e) {
-    firefeed.login(false, function(err, name) {
+    self._firefeed.login(false, function(err, name) {
       if (err) {
         alert("oops!");
       } else {
-        renderTimeline(name);
+        history.pushState(
+          {page: "timeline", name: name}, "timeline", "timeline"
+        );
+        self.renderTimeline(name);
       }
     });
     e.preventDefault();
   });
-}
+};
 
-function renderTimeline(name) {
+FirefeedUI.prototype.renderTimeline = function(name) {
   $("#header").html($("#tmpl-page-header").html());
   var content = Mustache.to_html($("#tmpl-timeline-content").html(), {
     name: name, location: "San Francisco, CA"
@@ -39,4 +67,4 @@ function renderTimeline(name) {
         id: userid, name: name
       })).appendTo("#suggested-users");
   });
-}
+};
