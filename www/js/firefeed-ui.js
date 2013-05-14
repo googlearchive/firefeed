3dc47fbaeaba729148044723f14553b058819a7f
Greg Soltis <gsoltis@gmail.com>
2013-05-13 17:26:42 -0700

    Flip over to use production resources

--- www/js/firefeed-ui.js
+++ www/js/firefeed-ui.js
@@ -8,7 +8,7 @@ function FirefeedUI() {
   this._limit = 141;
   this._loggedIn = false;
   this._spinner = new Spinner();
-  this._firefeed = new Firefeed("http://firefeed.fblocal.com:9000/");
+  this._firefeed = new Firefeed("https://firefeed.firebaseIO.com/");
   this._unload = null;
 
   // Setup page navigation.
